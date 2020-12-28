f784798c6fad697182384126dcb588b0849cf0d0
bikram109 <bikramsapkota99@gmail.com>
2020-12-28 15:30:52 +1000

    Initial Commit

---
+++ MVVMExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMExample
+//
+//  Created by Bikram Sapkota on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
